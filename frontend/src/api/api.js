e83a5469fdb3258c94f09f7d1d6c2f043d0847a4
mahithayy <hollamahitha@gmail.com>
2026-03-31 17:14:17 +0530

    added render link

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -4,12 +4,12 @@ export const uploadFile = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
 
-  const res = await axios.post("http://localhost:5000/upload", formData);
+  const res = await axios.post("https://mahi-website-navigator.onrender.com/upload", formData);
   return res.data;
 };
 
 export const checkIframeAllowed = async (url) => {
-  const res = await axios.get(`http://localhost:5000/check-frame?url=${encodeURIComponent(url)}`);
+  const res = await axios.get(`https://mahi-website-navigator.onrender.com/check-frame?url=${encodeURIComponent(url)}`);
   // Return the full object so the frontend can check offline status and HTTP codes
   return res.data;
 };
