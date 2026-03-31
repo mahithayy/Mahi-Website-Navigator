a7ff545d8c611cf085f6ae62c92c03999c12913e
mahithayy <hollamahitha@gmail.com>
2026-03-31 17:06:01 +0530

    removed hardcoded PORT for deployment

--- backend/server.js
+++ backend/server.js
@@ -80,4 +80,5 @@ app.get("/check-frame", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
