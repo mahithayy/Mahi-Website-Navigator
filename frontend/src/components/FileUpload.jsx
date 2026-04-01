7186bf973303b851328127ec76a4e8e2dd722f5c
mahithayy <hollamahitha@gmail.com>
2026-04-01 11:45:52 +0530

    Added a Remove button

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -1,45 +1,14 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { uploadFile } from "../api/api";
 
-// export default function FileUpload({ setUrls }) {
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState("");
 
-//   const handleUpload = async (e) => {
-//     const file = e.target.files[0];
-
-//     if (!file) {
-//       setError("Please select a file");
-//       return;
-//     }
-
-//     try {
-//       setLoading(true);
-//       setError("");
-
-//       const data = await uploadFile(file);
-
-//       if (!data.urls || data.urls.length === 0) {
-//         setError("No valid URLs found in file");
-//         return;
-//       }
-
-//       setUrls(data.urls);
-//     } catch (err) {
-//       // show backend error if available
-//       setError(err.response?.data?.error || "Upload failed");
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-// }
 
 
 export default function FileUpload({ setUrls }) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [fileName, setFileName] = useState("");
-
+const fileInputRef = useRef(null);//for clearing input
   const handleUpload = async (e) => {
     const file = e.target.files[0];
 
@@ -68,7 +37,17 @@ export default function FileUpload({ setUrls }) {
       setLoading(false);
     }
   };
+  //logic for "remove" button
+const handleRemove = () => {
+    setFileName("");
+    setUrls([]); // clear navigator
+    setError("");
 
+    // reset file input
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
+  };
   return (
     <div className="card-container file-upload-wrapper">
 
@@ -78,13 +57,33 @@ export default function FileUpload({ setUrls }) {
         Supported formats: <strong>.xlsx, .csv</strong>
       </p>
 
-      <input type="file" onChange={handleUpload} />
+      <input
+        type="file"
+        onChange={handleUpload}
+        ref={fileInputRef}
+        disabled={!!fileName}
+        className={fileName ? "file-input disabled" : "file-input"}
+      />
 
       {/* Selected file preview */}
       {fileName && (
-        <p className="file-name">📄 {fileName}</p>
+        <div><p style={{ color: "#16a34a", fontWeight: "600" }}>
+     File selected successfully
+  </p>
+        <p className="file-name">📄 {fileName}</p></div>
       )}
-
+{/*  REMOVE BUTTON */}
+      {fileName && (
+        <button
+          onClick={handleRemove}
+          style={{
+            background: "#ef4444",
+            marginTop: "10px"
+          }}
+        >
+           Remove File
+        </button>
+      )}
       {loading && <p className="status-loading">Uploading...</p>}
       {error && <p className="status-error">{error}</p>}
     </div>
